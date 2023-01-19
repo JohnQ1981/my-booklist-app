530fdb49e635894891ba5194cf6c37df2336d3fb
JohnQ1981 <ikram1981@gmail.com>
2023-01-18 20:50:21 -0600

    almost done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+
 
 function App() {
+  
+    var id =Math.round(Math.random() *1000);
+  
+  const [books, setBooks] = useState([]);
+  const createBook = (title) => {
+    //console.log("Need to add book with: ", title);
+    const updatedBooks =[...books,
+    {id:id,title:title}];
+    setBooks(updatedBooks);
+  };
+
   return (
-    <div className="App">
-      Hello
+    <div className="app">
+      <BookList books={books}/>
+      
+      <BookCreate onCreate={createBook} />
+
+      <div>{books.length}{" "}{'Random Rumber is: '}{" "}{id}</div>
+      
     </div>
   );
 }
